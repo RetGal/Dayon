9b913c574d8cdd60b5c0191db99ca920ab5e7ee7
RetGal <galante@puzzle.ch>
2018-06-20 00:05:29 +0200

    Added position and dimension classes

---
+++ src/mpo/dayon/common/gui/common/Dimension.java
@@ -0,0 +1,29 @@
+package mpo.dayon.common.gui.common;
+
+public class Dimension {
+	
+	private int width;
+	private int height;
+	
+	public Dimension(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+}
