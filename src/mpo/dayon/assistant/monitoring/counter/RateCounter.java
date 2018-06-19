35a5a0cd1128620da55bb72504641a6ffb371436
RetGal <galante@puzzle.ch>
2018-06-19 22:54:29 +0200

    Removed dead code

--- src/mpo/dayon/assistant/monitoring/counter/RateCounter.java
+++ src/mpo/dayon/assistant/monitoring/counter/RateCounter.java
@@ -1,12 +1,6 @@
 package mpo.dayon.assistant.monitoring.counter;
 
-import java.util.LinkedList;
-
 public abstract class RateCounter extends Counter<Double> {
-	/**
-	 * The last computed N instant rates : most recent is first (head).
-	 */
-	private final LinkedList<Double> instantRateHistory = new LinkedList<>();
 
 	private double instantValue = 0;
 
@@ -33,39 +27,12 @@ public abstract class RateCounter extends Counter<Double> {
 			if (instantStart.get() != -1) {
 				final long elapsed = System.currentTimeMillis() - instantStart.get();
 				rate = 1000.0 * instantValue / (double) elapsed;
-
-				synchronized (instantRateHistory) {
-					instantRateHistory.addFirst(rate);
-					if (instantRateHistory.size() > 10) {
-						instantRateHistory.removeLast();
-					}
-				}
-
 				instantStart.set(System.currentTimeMillis());
 				instantValue = 0;
 			}
 		}
 
 		fireOnInstantValueUpdated(rate);
-	}
-
-	/**
-	 * @return most recent is first in the returned array
-	 */
-	public double[] getInstantRateHistory() {
-		synchronized (instantRateHistory) {
-			final double[] history = new double[instantRateHistory.size()];
-
-			if (history.length > 0) {
-				int idx = 0;
-
-				for (Double rate : instantRateHistory) {
-					history[idx++] = rate;
-				}
-			}
-
-			return history;
-		}
 	}
 
 }
