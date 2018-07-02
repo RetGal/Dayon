e0f88541fbcb01d6ce99e504cda785e66f5896bd
RetGal <galante@puzzle.ch>
2018-07-02 21:57:06 +0200

    Enhanced desktop file

--- resources/setup.sh
+++ resources/setup.sh
@@ -26,30 +26,30 @@ chmod +x $INSTALL_DIR/bin/*.sh
 
 cat <<EOF > /usr/share/applications/DayonAssistant.desktop
 [Desktop Entry]
-Encoding=UTF-8
 Name=Dayon! Assistant
 Exec=$INSTALL_DIR/bin/dayon_assistant.sh
-Icon=$INSTALL_DIR/doc/html/favicon.ico
+Icon=$INSTALL_DIR/dayon.png
 Terminal=false
 Type=Application
-Categories=Network;Communication;
+Categories=Network;X-Communication;
 Comment=Offer remote assistance
 Comment[de]=Remotesupport anbieten
 Comment[fr]=Offrir assistance à distance
+Keywords=remote;support;offer help
 EOF
 
 cat <<EOF > /usr/share/applications/DayonAssisted.desktop
 [Desktop Entry]
-Encoding=UTF-8
 Name=Dayon! Assisted
 Exec=$INSTALL_DIR/bin/dayon_assisted.sh
-Icon=$INSTALL_DIR/doc/html/favicon.ico
+Icon=$INSTALL_DIR/dayon.png
 Terminal=false
 Type=Application
-Categories=Network;Communication;
+Categories=Network;X-Communication;
 Comment=Request remote assistance
 Comment[de]=Remotesupport erbitten
 Comment[fr]=Demander assistance à distance
+Keywords=remote;support;get help
 EOF
 
 echo "Dayon! setup finished successfully - happy sessions!"
