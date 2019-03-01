2f9ee0b60d28cbb82fdbd5c43f8020aa6437224f
RetGal <galante@puzzle.ch>
2019-03-01 21:05:16 +0100

    Added tests

---
+++ src/test/java/mpo/dayon/common/event/ListenersTest.java
@@ -0,0 +1,45 @@
+package mpo.dayon.common.event;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import mpo.dayon.assisted.capture.CaptureEngineListener;
+import mpo.dayon.assisted.compressor.CompressorEngine;
+import org.junit.jupiter.api.Test;
+
+class ListenersTest {
+
+    private final Listeners<CaptureEngineListener> listeners = new Listeners<>();
+
+    @Test
+    void shouldAddListener() {
+        // given
+        final CompressorEngine compressorEngine = new CompressorEngine();
+        assertTrue(listeners.getListeners().isEmpty());
+        // when
+        listeners.add(compressorEngine);
+        // then
+        assertEquals(1, listeners.getListeners().size());
+        assertEquals(compressorEngine, listeners.getListeners().get(0));
+    }
+
+    @Test
+    void shouldNotAddNullListener() {
+        // given
+        assertTrue(listeners.getListeners().isEmpty());
+        // when
+        listeners.add(null);
+        // then
+        assertTrue(listeners.getListeners().isEmpty());
+    }
+
+    @Test
+    void shouldNotFailOnRemoveNullListener() {
+        // given
+        assertTrue(listeners.getListeners().isEmpty());
+        // when
+        listeners.remove(null);
+        // then
+        assertTrue(listeners.getListeners().isEmpty());
+    }
+
+}
---
+++ src/test/java/mpo/dayon/common/version/VersionTest.java
@@ -0,0 +1,23 @@
+package mpo.dayon.common.version;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VersionTest {
+
+    @Test
+    void shouldObtainLatestRelease() {
+        // given
+        Version version = Version.get();
+
+        // when
+        String latestRelease = version.getLatestRelease();
+
+        // then
+        assertTrue(version.isNull());
+        assertFalse(version.isLatesVersion());
+        assertTrue(latestRelease.startsWith("v"));
+    }
+
+}
