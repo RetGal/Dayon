a8a1c3a7f698a1914670234555aa50af2dfd46eb
RetGal <galante@puzzle.ch>
2019-04-23 20:38:16 +0200

    Updated Jetty to the latest version

--- resources/bin/dayon.sh
+++ resources/bin/dayon.sh
@@ -5,6 +5,6 @@ DAYON_HOME=$(dirname "$0")/..
 JAVA_HOME=`ls -l /etc/alternatives/java | awk -F'> ' '{print $2}' | awk -F'/bin/java' '{print $1}'`
 JAVA=$JAVA_HOME/bin/java
 JAVA_OPTS="-Xms64M -Xmx256M"
-CLASSPATH="$DAYON_HOME/lib/dayon.jar:$DAYON_HOME/lib/jetty-http-9.4.15.v20190215.jar:$DAYON_HOME/lib/jetty-io-9.4.15.v20190215.jar:$DAYON_HOME/lib/jetty-server-9.4.15.v20190215.jar:$DAYON_HOME/lib/jetty-util-9.4.15.v20190215.jar:$DAYON_HOME/lib/javax.servlet-api-3.1.0.jar:$DAYON_HOME/lib/bzip2-0.9.1.jar:$DAYON_HOME/lib/grizzly-lzma-1.9.65.jar"
+CLASSPATH="$DAYON_HOME/lib/dayon.jar:$DAYON_HOME/lib/jetty-http-9.4.17.v20190418.jar:$DAYON_HOME/lib/jetty-io-9.4.17.v20190418.jar:$DAYON_HOME/lib/jetty-server-9.4.17.v20190418.jar:$DAYON_HOME/lib/jetty-util-9.4.17.v20190418.jar:$DAYON_HOME/lib/javax.servlet-api-3.1.0.jar:$DAYON_HOME/lib/bzip2-0.9.1.jar:$DAYON_HOME/lib/grizzly-lzma-1.9.65.jar"
 
 $JAVA $JAVA_OPTS -Ddayon.log=file -cp $CLASSPATH $1 $2
