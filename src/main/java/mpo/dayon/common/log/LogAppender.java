7b515acbac590c37997f210123c0bc56d2feac52
RetGal <galante@puzzle.ch>
2019-12-08 18:28:29 +0100

    Fixed date formatter bug

--- src/main/java/mpo/dayon/common/log/LogAppender.java
+++ src/main/java/mpo/dayon/common/log/LogAppender.java
@@ -8,12 +8,12 @@ import java.util.Date;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class LogAppender {
-	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss.SSS");
+	private final DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
 
 	protected String format(LogLevel level, @Nullable String message) {
 		message = (message == null) ? "" : message;
 		return String.format("[%20.20s] [%5.5s] (%s) %s", Thread.currentThread().getName(), level,
-				DATE_FORMAT.format(Date.from(Instant.now())), message);
+				dateFormat.format(Date.from(Instant.now())), message);
 	}
 
 	public void append(LogLevel level, @Nullable String message) {
