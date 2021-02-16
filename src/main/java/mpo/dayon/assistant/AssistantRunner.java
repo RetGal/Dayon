86c4494b6054b39f22a5219ef7e85d41af0d60f2
RetGal <galante@puzzle.ch>
2021-02-16 12:47:45 +0100

    Improved gui initialisation

--- src/main/java/mpo/dayon/assistant/AssistantRunner.java
+++ src/main/java/mpo/dayon/assistant/AssistantRunner.java
@@ -5,18 +5,22 @@ import mpo.dayon.assistant.gui.AssistantConfiguration;
 import mpo.dayon.common.Runner;
 import mpo.dayon.common.error.FatalErrorHandler;
 
+import javax.swing.SwingUtilities;
+
 class AssistantRunner implements Runner {
 	public static void main(String[] args) {
 		try {
 			Runner.overrideLocale(Runner.extractProgramArgs(args).get("lang"));
 			Runner.logAppInfo("dayon_assistant");
-
-			final Assistant assistant = new Assistant();
-			assistant.configure(new AssistantConfiguration());
-			assistant.start();
-
+			SwingUtilities.invokeLater(AssistantRunner::launchAssistant);
 		} catch (Exception ex) {
 			FatalErrorHandler.bye("The assistant is dead!", ex);
 		}
+	}
+
+	private static void launchAssistant() {
+		final Assistant assistant = new Assistant();
+		assistant.configure(new AssistantConfiguration());
+		assistant.start();
 	}
 }
--- src/main/java/mpo/dayon/assisted/AssistedRunner.java
+++ src/main/java/mpo/dayon/assisted/AssistedRunner.java
@@ -4,21 +4,24 @@ import mpo.dayon.assisted.gui.Assisted;
 import mpo.dayon.common.Runner;
 import mpo.dayon.common.error.FatalErrorHandler;
 
+import javax.swing.SwingUtilities;
 import java.util.Map;
 
 class AssistedRunner implements Runner {
-	public static void main(String[] args) {
-		try {
-			Map<String, String> programArguments = Runner.extractProgramArgs(args);
-			Runner.overrideLocale(programArguments.get("lang"));
-			Runner.logAppInfo("dayon_assisted");
+    public static void main(String[] args) {
+        try {
+            Map<String, String> programArgs = Runner.extractProgramArgs(args);
+            Runner.overrideLocale(programArgs.get("lang"));
+            Runner.logAppInfo("dayon_assisted");
+            SwingUtilities.invokeLater(() -> launchAssisted(programArgs.get("ah"), programArgs.get("ap")));
+        } catch (Exception ex) {
+            FatalErrorHandler.bye("The assisted is dead!", ex);
+        }
+    }
 
-			final Assisted assisted = new Assisted();
-			assisted.configure();
-			assisted.start(programArguments.get("ah"), programArguments.get("ap"));
-
-		} catch (Exception ex) {
-			FatalErrorHandler.bye("The assisted is dead!", ex);
-		}
-	}
+    private static void launchAssisted(String assistantHost, String assistantPort) {
+        final Assisted assisted = new Assisted();
+        assisted.configure();
+        assisted.start(assistantHost, assistantPort);
+    }
 }
