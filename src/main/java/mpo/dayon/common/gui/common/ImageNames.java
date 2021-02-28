c0d316d4e5648062eec0e2cf92aef87e0ec6321a
RetGal <galante@puzzle.ch>
2021-02-28 09:44:48 +0100

    Settings icon

--- src/main/java/mpo/dayon/common/gui/common/ImageNames.java
+++ src/main/java/mpo/dayon/common/gui/common/ImageNames.java
@@ -5,49 +5,28 @@ public abstract class ImageNames {
 	}
 
 	public static final String APP = "app_32.png";
-
 	public static final String APP_LARGE = "app_48.png";
-
-	public static final String COPY = "copy.png";
-
-	public static final String INFO = "info.png";
-
-	public static final String INFO_SMALL = "info_small.png";
-
-	public static final String START = "start.png";
-
-	public static final String STOP = "stop.png";
-
-	public static final String LNF = "looknfeel.png";
-
-	public static final String NETWORK_SETTINGS = "network_settings.png";
-
 	public static final String CAPTURE_SETTINGS = "capture_settings.png";
-
 	public static final String COMPRESSION_SETTINGS = "compression_settings.png";
-
-	public static final String RESET_CAPTURE = "reset_capture.png";
-
-	public static final String EXIT = "exit.png";
-
-	public static final String USERS = "users_48.png";
-
-	public static final String WAITING = "waiting.gif";
-
-	public static final String MOUSE_WHITE = "cursor_white.png";
-
-	public static final String MOUSE_YELLOW = "cursor_yellow.png";
-
 	public static final String CONTROL = "control.png";
-
-	public static final String HELP = "help.png";
-
-	public static final String UP = "up.png";
-
+	public static final String COPY = "copy.png";
 	public static final String DOWN = "down.png";
-
-	public static final String WIN = "win.png";
-
+	public static final String EXIT = "exit.png";
 	public static final String FIT = "fit.png";
+	public static final String HELP = "help.png";
+	public static final String INFO = "info.png";
+	public static final String INFO_SMALL = "info_small.png";
+	public static final String LNF = "looknfeel.png";
+	public static final String MOUSE_WHITE = "cursor_white.png";
+	public static final String MOUSE_YELLOW = "cursor_yellow.png";
+	public static final String NETWORK_SETTINGS = "network_settings.png";
+	public static final String RESET_CAPTURE = "reset_capture.png";
+	public static final String SETTINGS = "settings.png";
+	public static final String START = "start.png";
+	public static final String STOP = "stop.png";
+	public static final String UP = "up.png";
+	public static final String USERS = "users_48.png";
+	public static final String WAITING = "waiting.gif";
+	public static final String WIN = "win.png";
 
 }
