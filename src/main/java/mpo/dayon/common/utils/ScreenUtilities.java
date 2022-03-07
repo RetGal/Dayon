b84ded91836c5e040e712c394ae85b995db2d4cf
retgal <galante@puzzle.ch>
2022-03-07 19:57:02 +0100

    Fixed OutOfBoundsException

--- src/main/java/mpo/dayon/common/utils/ScreenUtilities.java
+++ src/main/java/mpo/dayon/common/utils/ScreenUtilities.java
@@ -5,6 +5,9 @@ import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 import mpo.dayon.common.capture.Gray8Bits;
+import mpo.dayon.common.log.Log;
+
+import static java.lang.String.format;
 
 public final class ScreenUtilities {
 
@@ -47,9 +50,14 @@ public final class ScreenUtilities {
 
     private static int[] captureRGB(Rectangle bounds) {
         BufferedImage image = robot.createScreenCapture(bounds);
+        final int imageHeight = image.getHeight();
+        final int imageWidth = image.getWidth();
+        if (imageHeight != bounds.height || imageWidth != bounds.width) {
+            Log.warn(format("Image dimensions %sx%s != bound dimensions %sx%s", imageHeight, imageWidth, bounds.height, bounds.width));
+        }
         int i = 0;
-        for (int yPos = bounds.y; yPos < bounds.height; yPos++) {
-            for (int xPos = bounds.x; xPos < bounds.width; xPos++) {
+        for (int yPos = bounds.y; yPos < imageHeight; yPos++) {
+            for (int xPos = bounds.x; xPos < imageWidth; xPos++) {
                 rgb[i++] = image.getRGB(xPos, yPos);
             }
         }
@@ -66,24 +74,19 @@ public final class ScreenUtilities {
 
     private static byte[] doRgbToGray8(Gray8Bits quantization, int[] rgb) {
         final byte[] xLevels = grays[quantization.ordinal()];
-
         int prevRgb = -1;
         byte prevGray = -1;
 
         for (int idx = 0; idx < rgb.length; idx++) {
             final int pixel = rgb[idx];
-
             if (pixel == prevRgb) {
                 gray[idx] = prevGray;
                 continue;
             }
-
             final int red = (pixel & 0x00FF0000) >> 16;
             final int green_blue = pixel & 0x0000FFFF;
             final int level = (red_levels[red] + green_blue_levels[green_blue]) >> 7;
-
             gray[idx] = xLevels[level];
-
             prevRgb = pixel;
             prevGray = gray[idx];
         }
@@ -91,21 +94,16 @@ public final class ScreenUtilities {
     }
 
     private static final short[] red_levels;
-
     private static final short[] green_blue_levels;
-
-    /**
-      Cache the conversion from red/green/blue into gray levels.
+    /*
+     Cache the conversion from red/green/blue into gray levels.
      */
     static {
         red_levels = new short[256];
-
         for (int red = 0; red < 256; red++) {
             red_levels[red] = (short) (128.0 * 0.212671 * red);
         }
-
         green_blue_levels = new short[256 * 256];
-
         for (int green = 0; green < 256; green++) {
             for (int blue = 0; blue < 256; blue++) {
                 green_blue_levels[(green << 8) + blue] = (short) ((128.0 * 0.715160 * green) + (128.0 * 0.072169 * blue));
@@ -114,29 +112,22 @@ public final class ScreenUtilities {
     }
 
     private static final byte[][] grays;
-
-    /**
-      Cache the quantization of all the gray levels (256).
+    /*
+     Cache the quantization of all the gray levels (256).
      */
     static {
         final Gray8Bits[] quantizations = Gray8Bits.values();
-
         grays = new byte[quantizations.length][];
-
         for (final Gray8Bits quantization : quantizations) {
             grays[quantization.ordinal()] = new byte[256];
-
             final int factor = 256 / quantization.getLevels();
-
             for (int idx = 0; idx < 256; idx++) {
                 // DOWN (0, 32, 64, 96, ...)
                 // levels[quantization.ordinal()][idx] = (byte) (idx / factor *
                 // factor);
-
                 // UP (31, 63,, 95, ...)
                 grays[quantization.ordinal()][idx] = (byte) (((1 + (idx / factor)) * factor) - 1);
             }
         }
     }
-
 }
