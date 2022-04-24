735558927969837763445484a9a4b2e91086771d
retgal <galante@puzzle.ch>
2022-04-24 15:46:57 +0200

    Prevent globbing and word splitting

--- resources/dayon_assistant.sh
+++ resources/dayon_assistant.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ABS_PATH=$(dirname $(realpath "$0"))
 if [ ! -f "$ABS_PATH/dayon.sh" ]; then
-  "$ABS_PATH/dayon/dayon.sh" mpo.dayon.assistant.AssistantRunner $1
+  "$ABS_PATH/dayon/dayon.sh" mpo.dayon.assistant.AssistantRunner "$1"
 else
-  "$ABS_PATH/dayon.sh" mpo.dayon.assistant.AssistantRunner $1
+  "$ABS_PATH/dayon.sh" mpo.dayon.assistant.AssistantRunner "$1"
 fi
--- resources/dayon_assisted.sh
+++ resources/dayon_assisted.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ABS_PATH=$(dirname $(realpath "$0"))
 if [ ! -f "$ABS_PATH/dayon.sh" ]; then
-  "$ABS_PATH/dayon/dayon.sh" mpo.dayon.assisted.AssistedRunner $1 $2 $3
+  "$ABS_PATH/dayon/dayon.sh" mpo.dayon.assisted.AssistedRunner "$1" "$2" "$3"
 else
-  "$ABS_PATH/dayon.sh" mpo.dayon.assisted.AssistedRunner $1 $2 $3
+  "$ABS_PATH/dayon.sh" mpo.dayon.assisted.AssistedRunner "$1" "$2" "$3"
 fi
