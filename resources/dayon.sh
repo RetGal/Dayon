952f3fc75371fcba8fc8786e9419b878d7a74f22
retgal <galante@puzzle.ch>
2022-05-09 07:33:26 +0200

    Use default shell script for flatpak

--- resources/dayon.sh
+++ resources/dayon.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 DAYON_HOME=$(dirname $(realpath "$0"))
-JAVA_HOME=`ls -l /etc/alternatives/java | awk -F'> ' '{print $2}' | awk -F'/bin/java' '{print $1}'`
-# Your favorite JRE/JDK 1.8+ ...
-#JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
-JAVA=${JAVA_HOME}/bin/java
+if which java >/dev/null; then
+    JAVA=$(which java)
+  else
+    JAVA_HOME=`ls -l /etc/alternatives/java | awk -F'> ' '{print $2}' | awk -F'/bin/java' '{print $1}'`
+    #JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre"
+    JAVA=${JAVA_HOME}/bin/java
+fi
 JAVA_OPTS=""
 CLASSPATH="$DAYON_HOME/dayon.jar"
 
