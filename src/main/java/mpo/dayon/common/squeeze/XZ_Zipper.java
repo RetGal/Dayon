ee8686c10e4a898772f1cf389814b7a0f6fb83fe
retgal <galante@puzzle.ch>
2022-06-24 22:44:20 +0200

    Use try-with-resource

--- src/main/java/mpo/dayon/common/squeeze/XZ_Zipper.java
+++ src/main/java/mpo/dayon/common/squeeze/XZ_Zipper.java
@@ -1,6 +1,5 @@
 package mpo.dayon.common.squeeze;
 
-
 import mpo.dayon.common.buffer.MemByteBuffer;
 import org.tukaani.xz.*;
 
@@ -9,25 +8,27 @@ import java.io.*;
 public class XZ_Zipper implements Zipper {
 	@Override
     public MemByteBuffer zip(MemByteBuffer unCompressed) throws IOException {
-		final MemByteBuffer compressed = new MemByteBuffer();
-		final XZOutputStream xzOutputStream = new XZOutputStream(compressed, new LZMA2Options(6));
-		xzOutputStream.write(unCompressed.getInternal(), 0, unCompressed.size());
-		xzOutputStream.flush();
-		xzOutputStream.close();
-		return compressed;
+		try (MemByteBuffer compressed = new MemByteBuffer()) {
+			try (XZOutputStream xzOutputStream = new XZOutputStream(compressed, new LZMA2Options(6))) {
+				xzOutputStream.write(unCompressed.getInternal(), 0, unCompressed.size());
+				xzOutputStream.flush();
+			}
+			return compressed;
+		}
 	}
 
 	@Override
     public MemByteBuffer unzip(MemByteBuffer compressed) throws IOException {
-		final MemByteBuffer unCompressed = new MemByteBuffer();
-		try (XZInputStream xzInputStream = new XZInputStream(new ByteArrayInputStream(compressed.getInternal(), 0, compressed.size()))) {
-			final byte[] buffer = new byte[4096];
-			int count;
-			while ((count = xzInputStream.read(buffer)) > 0) {
-				unCompressed.write(buffer, 0, count);
+		try (MemByteBuffer unCompressed = new MemByteBuffer()) {
+			try (XZInputStream xzInputStream = new XZInputStream(new ByteArrayInputStream(compressed.getInternal(), 0, compressed.size()))) {
+				final byte[] buffer = new byte[4096];
+				int count;
+				while ((count = xzInputStream.read(buffer)) > 0) {
+					unCompressed.write(buffer, 0, count);
+				}
 			}
+			// the flush and close methods of OutputStream do nothing
+			return unCompressed;
 		}
-		// the flush and close methods of OutputStream do nothing
-		return unCompressed;
 	}
 }
