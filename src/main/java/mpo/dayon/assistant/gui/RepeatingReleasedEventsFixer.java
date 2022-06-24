549171f13fd32c18a8fb89569eadf9946f7dd8cb
retgal <galante@puzzle.ch>
2022-06-24 22:52:16 +0200

    Fixing a singleton

--- src/main/java/mpo/dayon/assistant/gui/RepeatingReleasedEventsFixer.java
+++ src/main/java/mpo/dayon/assistant/gui/RepeatingReleasedEventsFixer.java
@@ -75,7 +75,7 @@ public final class RepeatingReleasedEventsFixer implements AWTEventListener {
 
     private final Map<Integer, ReleasedAction> _map = new HashMap<>();
 
-    private static RepeatingReleasedEventsFixer instance;
+    private static volatile RepeatingReleasedEventsFixer instance;
 
     private RepeatingReleasedEventsFixer() {
         // singleton
