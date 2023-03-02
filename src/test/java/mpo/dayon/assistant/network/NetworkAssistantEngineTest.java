38a7c5acb815b2abe6b8b1f6f9a863d836951769
retgal <galante@puzzle.ch>
2023-03-02 10:20:24 +0100

    Add tests

---
+++ src/test/java/mpo/dayon/assistant/network/NetworkAssistantEngineTest.java
@@ -0,0 +1,45 @@
+package mpo.dayon.assistant.network;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class NetworkAssistantEngineTest {
+
+    NetworkAssistantEngine engine;
+    NetworkAssistantEngineListener listener;
+
+    @BeforeEach
+    void init() {
+        engine = new NetworkAssistantEngine(null, null, null);
+        listener = mock(NetworkAssistantEngineListener.class);
+        engine.addListener(listener);
+    }
+
+    @Test
+    void testReconfigureStart() {
+        // given
+        engine.configure(new NetworkAssistantEngineConfiguration());
+        final NetworkAssistantEngineConfiguration configuration = new NetworkAssistantEngineConfiguration(12345);
+        engine.reconfigure(configuration);
+
+        // when
+        engine.start();
+
+        // then
+        verify(listener).onStarting(configuration.getPort());
+    }
+
+    @Test
+    void testCancel() {
+        // given
+
+        // when
+        engine.cancel();
+
+        // then
+        verify(listener).onDisconnecting();
+    }
+}
---
+++ src/test/java/mpo/dayon/assisted/network/NetworkAssistedEngineTest.java
@@ -0,0 +1,44 @@
+package mpo.dayon.assisted.network;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class NetworkAssistedEngineTest {
+
+    NetworkAssistedEngine engine;
+    NetworkAssistedEngineListener listener;
+
+    @BeforeEach
+    void init() {
+        engine = new NetworkAssistedEngine(null, null, null, null, null);
+        listener = mock(NetworkAssistedEngineListener.class);
+        engine.addListener(listener);
+    }
+
+    @Test
+    void testConnect() {
+        // given
+        final NetworkAssistedEngineConfiguration configuration = new NetworkAssistedEngineConfiguration();
+        engine.configure(configuration);
+
+        // when
+        engine.connect();
+
+        // then
+        verify(listener).onConnecting(configuration.getServerName(), configuration.getServerPort());
+    }
+
+    @Test
+    void testCancel() {
+        // given
+
+        // when
+        engine.cancel();
+
+        // then
+        verify(listener).onDisconnecting();
+    }
+}
