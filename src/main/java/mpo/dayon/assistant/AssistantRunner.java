792dc112c7cadc5513ed360914427f11ba79f532
retgal <galante@puzzle.ch>
2023-04-30 19:52:33 +0200

    Allow assistantRunner to be called directly

--- src/main/java/mpo/dayon/assistant/AssistantRunner.java
+++ src/main/java/mpo/dayon/assistant/AssistantRunner.java
@@ -3,10 +3,20 @@ package mpo.dayon.assistant;
 import mpo.dayon.assistant.gui.Assistant;
 import mpo.dayon.common.Runner;
 
+import java.util.Arrays;
+
 public class AssistantRunner {
 
 	public static void main(String[] args) {
-		Runner.main(args);
+		Runner.main(appendAssistant(args));
+	}
+
+	private static String[] appendAssistant(String[] args) {
+		String[] combined = Arrays.copyOf(args, args.length + 1);
+		String[] additional = new String[1];
+		additional[0] = "assistant";
+		System.arraycopy(additional, 0, combined, args.length, 1);
+		return combined;
 	}
 
 	public static void launchAssistant() {
