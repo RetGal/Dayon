b84061e34a2b7436bac4e855e2467299ae40c468
retgal <galante@puzzle.ch>
2023-11-14 00:00:22 +0100

    Reduce code smells

--- src/main/java/mpo/dayon/common/log/LogAppender.java
+++ src/main/java/mpo/dayon/common/log/LogAppender.java
@@ -5,12 +5,12 @@ import java.time.Instant;
 import java.util.Date;
 
 public abstract class LogAppender {
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss.SSS");
+	private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
 
 	protected String format(LogLevel level, String message) {
 		message = (message == null) ? "" : message;
 		return String.format("[%20.20s] [%5.5s] (%s) %s", Thread.currentThread().getName(), level,
-				DATE_FORMAT.format(Date.from(Instant.now())), message);
+				dateFormat.format(Date.from(Instant.now())), message);
 	}
 
 	public void append(LogLevel level, String message) {
