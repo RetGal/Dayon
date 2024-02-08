4acdab4451b9e8fe7af4305c8752c1f261b46caa
retgal <galante@puzzle.ch>
2024-02-08 10:31:11 +0100

    Add jna

--- src/main/java/mpo/dayon/assisted/AssistedRunner.java
+++ src/main/java/mpo/dayon/assisted/AssistedRunner.java
@@ -1,16 +1,23 @@
 package mpo.dayon.assisted;
 
+import com.sun.jna.platform.win32.Advapi32Util;
+import com.sun.jna.platform.win32.Win32Exception;
 import mpo.dayon.assisted.gui.Assisted;
 import mpo.dayon.common.Runner;
+import mpo.dayon.common.log.Log;
 
+import java.io.File;
 import java.util.Map;
 
 import static mpo.dayon.common.Runner.isAutoConnect;
 import static mpo.dayon.common.Runner.readPresetFile;
 
+import static com.sun.jna.platform.win32.WinReg.HKEY_LOCAL_MACHINE;
+
 public class AssistedRunner {
 
     public static void main(String[] args) {
+        fixUacBehaviour();
         Runner.main(args);
     }
 
@@ -23,6 +30,31 @@ public class AssistedRunner {
         } else {
             final Assisted assisted = new Assisted(null);
             assisted.start(assistantHost, assistantPort, true);
+        }
+    }
+
+    private static void fixUacBehaviour() {
+        if (File.separatorChar == '/') {
+            return;
+        }
+        final int off = 0x00000000;
+        final int on = 0x00000001;
+        final int secureDesktop = Advapi32Util.registryGetIntValue
+                (HKEY_LOCAL_MACHINE,
+                        "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System",
+                        "PromptOnSecureDesktop");
+        if (off != secureDesktop) {
+            try {
+                Advapi32Util.registrySetIntValue
+                        (HKEY_LOCAL_MACHINE,
+                                "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "PromptOnSecureDesktop", off);
+                Advapi32Util.registrySetIntValue
+                        (HKEY_LOCAL_MACHINE,
+                                "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", "EnableLUA", on);
+            } catch(Win32Exception e) {
+                Log.warn("Could not fix UAC behaviour, UAC dialogs will not be visible");
+                Log.warn("Rerun the assisted with admin rights to fix this");
+            }
         }
     }
 }
