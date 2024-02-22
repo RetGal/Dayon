05f73303b8884c835271f9e4a50aa17092766da4
retgal <galante@puzzle.ch>
2024-02-22 18:04:24 +0100

    Fix code smells

--- src/main/java/mpo/dayon/common/network/message/NetworkClipboardGraphicMessage.java
+++ src/main/java/mpo/dayon/common/network/message/NetworkClipboardGraphicMessage.java
@@ -18,6 +18,7 @@ public class NetworkClipboardGraphicMessage extends NetworkMessage {
         this.size = payload.getTransferData(DataFlavor.imageFlavor).getData().getDataBuffer().getSize() * 4;
     }
 
+    @java.lang.SuppressWarnings("squid:S5135") // assistant and assisted trust each other
     public static NetworkClipboardGraphicMessage unmarshall(ObjectInputStream in) throws IOException, ClassNotFoundException {
         TransferableImage graphic = (TransferableImage) in.readUnshared();
         return new NetworkClipboardGraphicMessage(graphic);
