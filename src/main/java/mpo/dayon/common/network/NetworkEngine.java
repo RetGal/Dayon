8b9aa36086a1bc8e005f6e5548f514f59cd5ed8a
retgal <galante@puzzle.ch>
2024-04-17 21:07:30 +0200

    Dont rely on default charset

--- src/main/java/mpo/dayon/common/network/NetworkEngine.java
+++ src/main/java/mpo/dayon/common/network/NetworkEngine.java
@@ -10,11 +10,11 @@ import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.StringSelection;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static mpo.dayon.common.network.message.NetworkMessageType.CLIPBOARD_FILES;
 import static mpo.dayon.common.network.message.NetworkMessageType.PING;
 import static mpo.dayon.common.utils.SystemUtilities.*;
@@ -57,7 +57,7 @@ public abstract class NetworkEngine {
      */
     public void sendClipboardText(String text) {
         if (sender != null) {
-            String utf8Encoded = StandardCharsets.UTF_8.decode(StandardCharsets.UTF_8.encode(text)).toString();
+            String utf8Encoded = UTF_8.decode(UTF_8.encode(text)).toString();
             sender.sendClipboardContentText(utf8Encoded, utf8Encoded.getBytes().length);
         }
     }
