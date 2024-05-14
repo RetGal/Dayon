46b62097650446568b139471bda15e213aff12f7
retgal <galante@puzzle.ch>
2024-05-14 22:10:00 +0200

    Fix and extend test

--- src/test/java/mpo/dayon/common/monitoring/counter/TileCounterTest.java
+++ src/test/java/mpo/dayon/common/monitoring/counter/TileCounterTest.java
@@ -2,6 +2,8 @@ package mpo.dayon.common.monitoring.counter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -11,13 +13,21 @@ class TileCounterTest {
     @Test
     void formatInstantValue() {
         // given
+        Locale defaultLocale = Locale.getDefault();
         int tiles = 300;
         int hits = 200;
         long sixtySix = ((long) tiles << 32) | hits;
         TileCounter tc= new TileCounter(null, null);
 
         // when, then
+        Locale.setDefault(Locale.forLanguageTag("de-CH"));
         assertEquals("300 (66.7%)", tc.formatInstantValue(sixtySix));
+
+        // when, then
+        Locale.setDefault(Locale.forLanguageTag("fr-FR"));
+        assertEquals("300 (66,7%)", tc.formatInstantValue(sixtySix));
+
+        Locale.setDefault(defaultLocale);
     }
 
     @Test
