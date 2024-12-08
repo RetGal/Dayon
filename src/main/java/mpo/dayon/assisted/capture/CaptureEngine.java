358cca47e37768b69f5399086bf324c6382d9a8d
retgal <galante@puzzle.ch>
2024-12-08 16:23:00 +0100

    Auto adjust tick

--- src/main/java/mpo/dayon/assisted/capture/CaptureEngine.java
+++ src/main/java/mpo/dayon/assisted/capture/CaptureEngine.java
@@ -2,6 +2,7 @@ package mpo.dayon.assisted.capture;
 
 import java.awt.*;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import mpo.dayon.common.capture.Capture;
@@ -53,11 +54,7 @@ public class CaptureEngine implements ReConfigurable<CaptureEngineConfiguration>
         this.thread = new Thread(new RunnableEx() {
             @Override
             protected void doRun() {
-                try {
-                    CaptureEngine.this.mainLoop();
-                } catch (InterruptedException e) {
-                    thread.interrupt();
-                }
+                CaptureEngine.this.mainLoop();
             }
         }, "CaptureEngine");
     }
@@ -94,7 +91,7 @@ public class CaptureEngine implements ReConfigurable<CaptureEngineConfiguration>
         thread.interrupt();
     }
 
-    private void mainLoop() throws InterruptedException {
+    private void mainLoop() {
         Gray8Bits quantization = null;
         boolean captureColors = false;
         int tick = -1;
@@ -145,11 +142,15 @@ public class CaptureEngine implements ReConfigurable<CaptureEngineConfiguration>
             skipped = syncOnTick(start, captureCount, captureId, tick);
             captureCount += skipped;
             captureId += skipped;
+            if (skipped > 0) {
+                tick +=10;
+                Log.info("Increased capture tick to " + tick);
+            }
         }
         Log.info("The capture engine has been stopped!");
     }
 
-    private static int syncOnTick(final long start, final int captureCount, final int captureId, final long tick) throws InterruptedException {
+    private static int syncOnTick(final long start, final int captureCount, final int captureId, final long tick) {
         int delayedCaptureCount = 0;
         while (true) {
             final long captureMaxEnd = start + (captureCount + delayedCaptureCount) * tick;
@@ -158,7 +159,11 @@ public class CaptureEngine implements ReConfigurable<CaptureEngineConfiguration>
                 ++delayedCaptureCount;
                 Log.warn(format("Skipping capture (%d) %s", captureId + delayedCaptureCount, UnitUtilities.toElapsedTime(-capturePause)));
             } else if (capturePause > 0) {
-                Thread.sleep(capturePause);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(capturePause);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
                 return delayedCaptureCount;
             }
         }
