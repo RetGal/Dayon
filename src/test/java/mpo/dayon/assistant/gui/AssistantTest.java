2d9b8deca1b7371730233e7319ac56ed3dd44884
retgal <galante@puzzle.ch>
2025-03-23 22:52:53 +0100

    Ignore on headless

--- src/test/java/mpo/dayon/assistant/gui/AssistantTest.java
+++ src/test/java/mpo/dayon/assistant/gui/AssistantTest.java
@@ -2,6 +2,8 @@ package mpo.dayon.assistant.gui;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static mpo.dayon.common.configuration.Configuration.DEFAULT_TOKEN_SERVER_URL;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,31 +11,37 @@ class AssistantTest {
 
     @Test
     void withCustomTokenServerUrlFromYaml() {
-        // given
-        Assistant assistant = new Assistant("https://rvs.example.com", null);
+        if (!GraphicsEnvironment.isHeadless()) {
+            // given
+            Assistant assistant = new Assistant("https://rvs.example.com", null);
 
-        // when then
-        assertTrue(assistant.hasTokenServerUrlFromYaml());
-        assertEquals("https://rvs.example.com", System.getProperty("dayon.custom.tokenServer"));
+            // when then
+            assertTrue(assistant.hasTokenServerUrlFromYaml());
+            assertEquals("https://rvs.example.com", System.getProperty("dayon.custom.tokenServer"));
+        }
     }
 
     @Test
     void withDefaultTokenServerUrlFromYaml() {
-        // given
-        Assistant assistant = new Assistant(DEFAULT_TOKEN_SERVER_URL, null);
+        if (!GraphicsEnvironment.isHeadless()) {
+            // given
+            Assistant assistant = new Assistant(DEFAULT_TOKEN_SERVER_URL, null);
 
-        // when then
-        assertTrue(assistant.hasTokenServerUrlFromYaml());
-        assertNull(System.getProperty("dayon.custom.tokenServer"));
+            // when then
+            assertTrue(assistant.hasTokenServerUrlFromYaml());
+            assertNull(System.getProperty("dayon.custom.tokenServer"));
+        }
     }
 
     @Test
     void withoutTokenServerUrl() {
-        // given
-        Assistant assistant = new Assistant(null, null);
+        if (!GraphicsEnvironment.isHeadless()) {
+            // given
+            Assistant assistant = new Assistant(null, null);
 
-        // when then
-        assertFalse(assistant.hasTokenServerUrlFromYaml());
-        assertNull(System.getProperty("dayon.custom.tokenServer"));
+            // when then
+            assertFalse(assistant.hasTokenServerUrlFromYaml());
+            assertNull(System.getProperty("dayon.custom.tokenServer"));
+        }
     }
 }
