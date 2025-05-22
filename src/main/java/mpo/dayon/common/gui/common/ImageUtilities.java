c5cd95021ad5746ad8ea5604490d61b226382424
Reto Galante <galante@puzzle.ch>
2025-05-22 09:18:27 +0200

    Fix multires image generation

--- src/main/java/mpo/dayon/common/gui/common/ImageUtilities.java
+++ src/main/java/mpo/dayon/common/gui/common/ImageUtilities.java
@@ -7,7 +7,6 @@ import java.awt.image.BaseMultiResolutionImage;
 import java.io.IOException;
 import java.util.*;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
@@ -18,26 +17,25 @@ public final class ImageUtilities {
     private static final Map<String, ImageIcon> ICON_CACHE = new HashMap<>();
 
     public static ImageIcon getOrCreateIcon(String name) {
-        return ICON_CACHE.computeIfAbsent(name, key -> {
+        ImageIcon icon = ICON_CACHE.get(name);
+        if (icon == null) {
             final String rname = "/images/%s/" + name;
             try {
-                return key.equals("waiting.gif")
-                        ? new ImageIcon(ImageUtilities.class.getResource("/images/" + name))
-                        : new ImageIcon(new BaseMultiResolutionImage(getImages(format(rname, "100", "125", "150", "175", "200")).toArray(new Image[0])));
-            } catch (IllegalArgumentException ex) {
+                if (!name.equals("waiting.gif")) {
+                    List<Image> imgList = new ArrayList<>();
+                    for (String s : Arrays.asList("100", "125", "150", "175", "200")) {
+                        imgList.add(ImageIO.read(ImageUtilities.class.getResource(format(rname, s))));
+                    }
+                    BaseMultiResolutionImage multiResolutionImage = new BaseMultiResolutionImage(imgList.toArray(new Image[0]));
+                    icon = new ImageIcon(multiResolutionImage);
+                } else {
+                    icon = new ImageIcon(ImageUtilities.class.getResource("/images/" + name));
+                }
+            } catch (IOException | IllegalArgumentException ex) {
                 throw new IllegalStateException(format("Missing icon [%s].", rname));
             }
-        });
+            ICON_CACHE.put(name, icon);
+        }
+        return icon;
     }
-
-    private static List<Image> getImages(String... paths) {
-        return Arrays.stream(paths).map(path -> {
-            try {
-                return ImageIO.read(ImageUtilities.class.getResource(path));
-            } catch (IOException ex) {
-                throw new IllegalArgumentException(ex);
-            }
-        }).collect(Collectors.toList());
-    }
-
 }
