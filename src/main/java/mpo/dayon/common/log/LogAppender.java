34c138dedeced61bba6a9454fb8280d7a038c665
retgal <galante@puzzle.ch>
2026-08-25 22:29:09 +0200

    Improve LogAppender

--- src/main/java/mpo/dayon/common/log/LogAppender.java
+++ src/main/java/mpo/dayon/common/log/LogAppender.java
@@ -1,15 +1,27 @@
 package mpo.dayon.common.log;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public abstract class LogAppender {
-	private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("HH:mm:ss.SSS").withZone(ZoneId.systemDefault());
+	private static final ThreadLocal<SimpleDateFormat> dateFormat = ThreadLocal.withInitial(() ->
+		new SimpleDateFormat("HH:mm:ss.SSS"));
+	private static final ThreadLocal<StringBuilder> builder = ThreadLocal.withInitial(StringBuilder::new);
 
 	protected String format(LogLevel level, String message) {
-		return String.format("[%20.20s] [%5.5s] (%s) %s", Thread.currentThread().getName(), level,
-				dateFormat.format(Instant.now()), (message == null) ? "" : message);
+		StringBuilder sb = builder.get();
+		sb.setLength(0);
+
+		String threadName = Thread.currentThread().getName();
+		sb.append(String.format("[%20.20s]", threadName));
+		sb.append(String.format(" [%5.5s]", level));
+		sb.append(" (");
+		sb.append(dateFormat.get().format(new Date()));
+		sb.append(") ");
+		if (message != null) {
+			sb.append(message);
+		}
+		return sb.toString();
 	}
 
 	public void append(LogLevel level, String message) {
