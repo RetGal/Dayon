49952a7dcd34814dfa3d2fbffa917244d8617287
retgal <galante@puzzle.ch>
2026-08-30 00:27:09 +0200

    Mute errors

--- ext/whatismyip.php
+++ ext/whatismyip.php
@@ -5,11 +5,11 @@ if (!empty($_REQUEST['p'])) {
     $address = $_SERVER['REMOTE_ADDR'];
     $type = filter_var($address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) ? AF_INET : AF_INET6;
     $port = substr($_REQUEST['p'], 0, 5);
-    $socket=socket_create($type, SOCK_STREAM, SOL_TCP);
+    $socket = @socket_create($type, SOCK_STREAM, SOL_TCP);
     if ($socket &&
-        socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array('sec'=>0, 'usec'=>500)) &&
-        socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec'=>0, 'usec'=>500)) &&
-        socket_connect($socket, $address, $port)) {
+        @socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array('sec'=>0, 'usec'=>500)) &&
+        @socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec'=>0, 'usec'=>500)) &&
+        @socket_connect($socket, $address, $port)) {
         echo 1;
     } else {
         echo 0;
